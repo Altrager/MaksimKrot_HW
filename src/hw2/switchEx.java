2678b880347504be0a43abb0aa3071ecc6a3e21e
Maxim Krot <maks.crot@gmail.com>
2020-04-15 22:29:04 +0300

    second

---
+++ src/hw2/switchEx.java
@@ -0,0 +1,35 @@
+package hw2;
+
+
+public class switchEx {
+    public static void main(String[] args) {
+//не знаю, как задать условие, что n < 10. Надеюсь на помощь.
+        int n < 10;
+        switch (n){
+            case 1:
+                System.out.println("Понедельник!");
+                break;
+            case 2:
+                System.out.println("Вторник!");
+                break;
+            case 3:
+                System.out.println("Среда!");
+                break;
+            case 4:
+                System.out.println("Четверг!");
+                break;
+            case 5:
+                System.out.println("Пятница!");
+                break;
+            case 6:
+                System.out.println("Суббота!");
+                break;
+            case 7:
+                System.out.println("Воскресенье!");
+                break;
+            default:
+                System.out.println("Введенное число некорректно!");
+
+        }
+    }
+}
