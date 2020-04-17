b4591bf07f56b8ac15815e5ad91da1a68c6991c5
Maxim Krot <maks.crot@gmail.com>
2020-04-17 09:22:29 +0300

    double array

---
+++ src/hw2/doubleArray.java
@@ -0,0 +1,16 @@
+package hw2;
+
+import java.util.Scanner;
+
+public class doubleArray {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println("Введите число: ");
+
+        int[][] arrayDouble = new int[n][n];
+        System.out.println(arrayDouble);
+
+// с дальнейшим условием не разобрался
+    }
+}
